9c229c5092f350429884b90a91e79c69830caa68
sykim1106 <sykim1106@naver.com>
2022-07-14 16:28:27 +0900

    주석 처리 및 refactoring

--- predict_module.py
+++ predict_module.py
@@ -24,8 +24,10 @@ def window_image(dcm, window_center, window_width):
 # Save different images as R, G, B: in 3 dimensions
 # input : Dicom 데이터셋 인스턴스
 # output : 3-channel 이미지
-def convert_dcm2img_3ch(dcm):
-    brain_img = window_image(dcm, 40, 80)
+def convert_dcm2img_3ch(dcm):                                                                   # CT 이미지는 HU(Hounsfield Unit)을 따른다. 
+                                                                                                # Dicom file 은 1-channel 이고, 모델은 3-channel 을 입력받으므로 변환을 할 때, 최대한 의미있게 변환하고자,
+                                                                                                # 연조직, 경막하출혈, 뇌 이렇게 3개로 나눠서 3-channel 이미지로 변환한다
+    brain_img = window_image(dcm, 40, 80)                                                       # HU 에서 일반적으로 각각의 픽셀값 범위들이 알려져있다. 그에 맞춰서 추출한다.
     subdural_img = window_image(dcm, 80, 200)
     soft_img = window_image(dcm, 40, 380)
 
@@ -43,14 +45,13 @@ def convert_dcm2img_3ch(dcm):
 
 def dicom2nparray(dcm_file_path):
     dcm_file = dicom.read_file(
-        dcm_file_path)  # 해당 파일 위치에 있는 dicom file READ : dcm_file 은 dicom 데이터셋 인스턴스이다. 필드값으로 {pixel_array, patient_id, study_date ... } 등이 있음
-    dcm_pixel = dcm_file.pixel_array  # pixel_array 를 받아와서 변수에 저장한다.
+        dcm_file_path)                                                                             # 해당 파일 위치에 있는 dicom file READ : dcm_file 은 dicom 데이터셋 인스턴스이다. 필드값으로 {pixel_array, patient_id, study_date ... } 등이 있음
+    dcm_pixel = dcm_file.pixel_array                                                               # pixel_array 를 받아와서 변수에 저장한다.
 
-    img_array = (dcm_pixel - np.min(dcm_pixel)) / (np.max(dcm_pixel) - np.min(
-        dcm_pixel)) * 255.0  # DICOM 파일은 픽셀값의 범위가 약 -1000 ~ 약 3000 이다. 픽셀값 범위를 0 ~ 255 범위로 정규화 시키겠다.
+    img_array = (dcm_pixel - np.min(dcm_pixel)) / (np.max(dcm_pixel) - np.min(dcm_pixel)) * 255.0  # DICOM 파일은 픽셀값의 범위가 약 -1000 ~ 약 3000 이다. 픽셀값 범위를 0 ~ 255 범위로 정규화 시키겠다.
     img_array = cv2.resize(img_array,
-                           (224, 224))  # img_array 는 사이즈가 (512,512) 이므로, ResNet 에 입력하기 위해 (224,224) 로 resize 한다.
-    # plt.imshow(dcm_pixel , cmap="bone")
+                           (224, 224))                                                             # img_array 는 사이즈가 (512,512) 이므로, ResNet 에 입력하기 위해 (224,224) 로 resize 한다.
+
     return img_array  # numpy_array 반환
 
 
@@ -87,37 +88,35 @@ def preprocess(dcm_file_path):
 
 
 def predict_and_lime(img_preprocessed, img_original):
-    # 모델 불러오기
-    loaded = keras.models.load_model('../resnet50_0712_model.h5')
+   
+    loaded = keras.models.load_model('../resnet50_0712_model.h5')                       # 모델 로드
 
-    ml_result = loaded.predict(img_preprocessed)[0][
-        0]  # 모델의 결과값 받아오기, 참고로 뒤에 sigmoid 함수를 붙였으므로 0에 가까운 값 or 1에 가까운 값이 출력된다
+    ml_result = loaded.predict(img_preprocessed)[0][0]                                  # 모델의 결과값 받아오기, 참고로 뒤에 sigmoid 함수를 붙였으므로 0에 가까운 값 or 1에 가까운 값이 출력된다
 
-    ml_result = "hemorrhage" if ml_result < 0.5 else "normal"
+    ml_result = "hemorrhage" if ml_result < 0.5 else "normal"                           # 마지막은 sigmoid 를 거쳐서 나온 0 ~ 1 사이 값이므로 0.5 를 기준으로 정상, 뇌출혈로 분류한다.
 
-    if ml_result == "hemorrhage":  # 뇌출혈로 예측할 때에만 LIME 실행
+    if ml_result == "hemorrhage":                                                       # 뇌출혈로 예측할 때에만 LIME 실행
 
-        explainer = lime_image.LimeImageExplainer(verbose=False)  # Lime 인스턴스 생성
+        explainer = lime_image.LimeImageExplainer(verbose=False)                        # Lime 인스턴스 생성
 
         explanation = explainer.explain_instance(
-            img_preprocessed[0],
-            classifier_fn=loaded.predict,
-            top_labels=5,
-            hide_color=0,
-            num_samples=50
+            img_preprocessed[0],                                                        # Lime 을 적용할 input 이미지
+            classifier_fn=loaded.predict,                                               # 분류기 
+            top_labels=5,                                                               # 샘플링 한 것 중에서 고를 최상의 Lime 라벨링 갯수
+            hide_color=0,                                                               # 색상을 숨길지 여부
+            num_samples=50                                                              # Lime 이 만들 가상의 샘플 갯수(샘플 갯수에 따라 연산속도가 증가한다.)
         )
 
-        temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=1,
-                                                    hide_rest=False)  # lime 의 이미지랑 마스크 받아오기
+        temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=1, hide_rest=False)  # lime 의 이미지랑 마스크 받아오기
 
-        img_colored = np.expand_dims(img_original, axis=-1)  # 원본 이미지로에 축 하나 추가 : BATCH
-        img_colored = cv2.cvtColor(np.uint8(img_colored), cv2.COLOR_GRAY2RGB)  # 컬러 이미지로 변환
-        img_colored[mask != 0] = [255, 0, 0]  # 마스크된 부분만 빨간색으로 색칠
+        img_colored = np.expand_dims(img_original, axis=-1)                                                                           # 원본 이미지에 축 하나 추가 : BATCH
+        img_colored = cv2.cvtColor(np.uint8(img_colored), cv2.COLOR_GRAY2RGB)                                                         # 컬러 이미지로 변환
+        img_colored[mask != 0] = [255, 0, 0]                                                                                          # Lime 마스크된 부분만 빨간색으로 색칠
 
         return ml_result, img_colored
 
     else:
-        return ml_result, None
+        return ml_result, None                                                                                                        # 정상인 경우에는 Lime 을 실행하지 않으므로, None 을 반환  
 
 
 '''# 예시 코드
